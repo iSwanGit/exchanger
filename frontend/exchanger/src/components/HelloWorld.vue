2692a4a4b63b6f3ed72a9b1d68ef4fbfec2afc9e
saru <sarguments@gmail.com>
2020-09-20 16:33:24 +0900

    프론트 bootstrap-vue 적용, 환율 부분 필터 추가, 에러메시지 추가

--- frontend/exchanger/src/components/HelloWorld.vue
+++ frontend/exchanger/src/components/HelloWorld.vue
@@ -1,31 +1,57 @@
 <template>
   <div>
     <h1>환율 계산</h1>
-    <form>
-      <div>송금국가: 미국(USD)</div>
-      <div>
-        <label for="currencySelect">수취국가: </label>
-        <select id="currencySelect" name="Currency" @change="onChange($event)" class="form-control"
-                v-model="targetCurrency">
-          <option value="krw" selected>한국(KRW)</option>
-          <option value="jpy">일본(JPY)</option>
-          <option value="php">필리핀(PHP)</option>
-        </select>
-      </div>
-      <div>
-        환율 : {{ currency }} {{ targetCurrency.toUpperCase() }}/USD
-      </div>
-      <div>
-        <label for="amount">송금액: </label>
-        <input id="amount" type="text" v-model="amount"> USD
-      </div>
-      <div>
-        <button type="submit" @click.prevent="calculate">Submit</button>
-      </div>
-    </form>
+
+    <b-form @submit.prevent="calculate">
+      <b-form-group
+        label="송금국가: "
+        label-for="from"
+        label-cols-sm="4"
+        label-cols-lg="3"
+      >
+        <b-form-input
+          id="from"
+          type="text"
+          value="미국(USD)"
+          readonly
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        label="수취국가: "
+        label-for="currencySelect"
+        label-cols-sm="4"
+        label-cols-lg="3"
+      >
+        <b-form-select
+          id="currencySelect"
+          v-model="targetCurrency" :options="options" @change="onChange"
+        ></b-form-select>
+      </b-form-group>
+      <b-form-group
+        label="환율 : "
+        label-for="result"
+        label-cols-sm="4"
+        label-cols-lg="3"
+      >
+        <div>{{ currency | currencyFilter }} {{ targetCurrency.toUpperCase() }}/USD</div>
+      </b-form-group>
+      <b-form-group id="input-group-2" label="송금액 : " label-for="amount"
+                    label-cols-sm="4" label-cols-lg="3">
+        <b-input-group>
+          <b-input id="amount" v-model="amount"></b-input><span>USD</span>
+        </b-input-group>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+
+    <hr>
 
     <div v-if="result">
-      수취금액은 {{ result }} 입니다.
+      수취금액은 {{ result | currencyFilter }} {{ targetCurrency.toUpperCase() }} 입니다.
+    </div>
+    <div v-if="message">
+      <b-alert show variant="danger">{{ message }}</b-alert>
     </div>
   </div>
 </template>
@@ -38,6 +64,11 @@ export default {
   data() {
     return {
       targetCurrency: 'krw',
+      options: [
+        { value: 'krw', text: '한국(KRW)' },
+        { value: 'jpy', text: '일본(JPY)' },
+        { value: 'php', text: '필리핀(PHP)' },
+      ],
       currency: '',
       amount: '',
       dummy: {
@@ -47,12 +78,12 @@ export default {
         php: 50,
       },
       result: null,
-      message: '',
+      message: null,
     };
   },
   methods: {
     onChange(event) {
-      this.targetCurrency = event.target.value;
+      this.targetCurrency = event;
       this.fetchData();
     },
     fetchData() {
--- frontend/exchanger/src/main.js
+++ frontend/exchanger/src/main.js
@@ -1,5 +1,21 @@
 import Vue from 'vue';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
+import VueCurrencyFilter from 'vue-currency-filter';
 import App from './App.vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+Vue.use(VueCurrencyFilter, {
+  name: 'currencyFilter',
+  thousandsSeparator: ',',
+  fractionCount: 2,
+  fractionSeparator: '.',
+  symbolPosition: 'back',
+  symbolSpacing: true,
+});
 
 Vue.config.productionTip = false;
 
